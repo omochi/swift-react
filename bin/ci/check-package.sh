ba5b3f657635ed9193c9e400990d977ea2f5927d
omochimetaru <omochi.metaru@gmail.com>
2024-04-17 09:32:00 +0900

    update check

--- bin/ci/check-package.sh
+++ bin/ci/check-package.sh
@@ -3,5 +3,5 @@ set -ueo pipefail
 cd "$(dirname "$0")/../.."
 
 set -x
-grep -q 'usesJavaScriptKitMockOnMac = true' Package.swift
+grep -q 'usesJavaScriptKitMockOnMac = false' Package.swift
 grep -q 'usesLocalJavaScriptKit = false' Package.swift
